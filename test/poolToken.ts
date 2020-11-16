cdc4f3ea1652f8a2f5e176a7fe32a7805771efc9
puercha-cc <61304045+puercha-cc@users.noreply.github.com>
2020-11-16 15:53:48 +0800

    commit
    poolToken test finished

--- test/poolToken.ts
+++ test/poolToken.ts
@@ -36,101 +36,101 @@ describe("poolToken", function () {
     })) as TestPcToken;
   });
 
-  // describe("token metadata", async () => {
-  //   it("Should have 18 decimals", async () => {
-  //     const decimals = await pcToken.decimals();
-  //     expect(decimals).to.equal(18);
-  //   });
-  //   it("Token name should be correct", async () => {
-  //     const name = await pcToken.name();
-  //     // console.log("name is: ",name);
-  //     expect(name).to.eq(NAME);
-  //   });
-  //   it("Symbol should be correct", async () => {
-  //     const symbol = await pcToken.symbol();
-  //     expect(symbol).to.eq(SYMBOL);
-  //   });
-  //   it("Initial supply should be zero", async () => {
-  //     const totalSupply = await pcToken.totalSupply();
-  //     // console.log("totalSupply is: ",totalSupply); -- BigNumber { _hex: '0x00' }
-  //     expect(totalSupply).to.eq(0);
-  //   });
-  //   it("After minting total supply should go up by minted amount", async () => {
-  //     // 增加量
-  //     const mintAmount = constants.WeiPerEther.mul(2);
-  //     // Mint in two tx to check if that works
-  //     await pcToken.mint(account, mintAmount.div(2));
-  //     await pcToken.mint(account, mintAmount.div(2));
+  describe("token metadata", async () => {
+    it("Should have 18 decimals", async () => {
+      const decimals = await pcToken.decimals();
+      expect(decimals).to.equal(18);
+    });
+    it("Token name should be correct", async () => {
+      const name = await pcToken.name();
+      // console.log("name is: ",name);
+      expect(name).to.eq(NAME);
+    });
+    it("Symbol should be correct", async () => {
+      const symbol = await pcToken.symbol();
+      expect(symbol).to.eq(SYMBOL);
+    });
+    it("Initial supply should be zero", async () => {
+      const totalSupply = await pcToken.totalSupply();
+      // console.log("totalSupply is: ",totalSupply); -- BigNumber { _hex: '0x00' }
+      expect(totalSupply).to.eq(0);
+    });
+    it("After minting total supply should go up by minted amount", async () => {
+      // 增加量
+      const mintAmount = constants.WeiPerEther.mul(2);
+      // Mint in two tx to check if that works
+      await pcToken.mint(account, mintAmount.div(2));
+      await pcToken.mint(account, mintAmount.div(2));
 
-  //     const totalSupply = await pcToken.totalSupply();
-  //     // console.log("mintamount",mintAmount) --  BigNumber { _hex: '0x1bc16d674ec80000' }
-  //     expect(totalSupply).to.eq(mintAmount);
-  //   });
-  //   it("Burning tokens should lower the total supply", async () => {
-  //     // 烧毁代币会降低总供应量
-  //     const mintAmount = constants.WeiPerEther.mul(2);
-  //     await pcToken.mint(account, mintAmount);
-  //     await pcToken.burn(account, mintAmount.div(2));
-  //     const totalSupply = await pcToken.totalSupply();
-  //     expect(totalSupply).to.eq(mintAmount.div(2));
-  //   });
-  //   it("Burning more than an address's balance should fail", async () => {
-  //     const mintAmount = constants.WeiPerEther;
-  //     await pcToken.mint(account, mintAmount);
-  //     await expect(pcToken.burn(account, constants.WeiPerEther.add(1))).to.be.revertedWith(
-  //       "ERR_INSUFFICIENT_BAL"
-  //     );
-  //   });
-  // });
-  // describe("balanceOf", async () => {
-  //   it("Should return zero if no balance", async () => {
-  //     const balance = await pcToken.balanceOf(account);
-  //     // console.log("0balance:",balance) -- BigNumber { _hex: '0x00' }
-  //     expect(balance).to.eq(0);
-  //   });
-  //   it("Should return correct amount if account has some tokens", async () => {
-  //     const mintAmount = constants.WeiPerEther.mul(2);
-  //     await pcToken.mint(account, mintAmount);
-  //     const balance = await pcToken.balanceOf(account);
-  //     // console.log("balanceamount:",balance) -- BigNumber { _hex: '0x1bc16d674ec80000' }
-  //     expect(balance).to.eq(mintAmount);
-  //   });
-  // });
-  // describe("transfer", async () => {
-  //   it("Should fail when the sender does not have enought balance", async () => {
-  //     await pcToken.mint(account, constants.WeiPerEther);
-  //     await expect(pcToken.transfer(account2, constants.WeiPerEther.add(1))).to.be.revertedWith(
-  //       "ERR_INSUFFICIENT_BAL"
-  //     );
-  //   });
-  //   it("Sending the entire balance should work", async () => {
-  //     await pcToken.mint(account, constants.WeiPerEther);
-  //     await pcToken.transfer(account2, constants.WeiPerEther);
+      const totalSupply = await pcToken.totalSupply();
+      // console.log("mintamount",mintAmount) --  BigNumber { _hex: '0x1bc16d674ec80000' }
+      expect(totalSupply).to.eq(mintAmount);
+    });
+    it("Burning tokens should lower the total supply", async () => {
+      // 烧毁代币会降低总供应量
+      const mintAmount = constants.WeiPerEther.mul(2);
+      await pcToken.mint(account, mintAmount);
+      await pcToken.burn(account, mintAmount.div(2));
+      const totalSupply = await pcToken.totalSupply();
+      expect(totalSupply).to.eq(mintAmount.div(2));
+    });
+    it("Burning more than an address's balance should fail", async () => {
+      const mintAmount = constants.WeiPerEther;
+      await pcToken.mint(account, mintAmount);
+      await expect(pcToken.burn(account, constants.WeiPerEther.add(1))).to.be.revertedWith(
+        "ERR_INSUFFICIENT_BAL"
+      );
+    });
+  });
+  describe("balanceOf", async () => {
+    it("Should return zero if no balance", async () => {
+      const balance = await pcToken.balanceOf(account);
+      // console.log("0balance:",balance) -- BigNumber { _hex: '0x00' }
+      expect(balance).to.eq(0);
+    });
+    it("Should return correct amount if account has some tokens", async () => {
+      const mintAmount = constants.WeiPerEther.mul(2);
+      await pcToken.mint(account, mintAmount);
+      const balance = await pcToken.balanceOf(account);
+      // console.log("balanceamount:",balance) -- BigNumber { _hex: '0x1bc16d674ec80000' }
+      expect(balance).to.eq(mintAmount);
+    });
+  });
+  describe("transfer", async () => {
+    it("Should fail when the sender does not have enought balance", async () => {
+      await pcToken.mint(account, constants.WeiPerEther);
+      await expect(pcToken.transfer(account2, constants.WeiPerEther.add(1))).to.be.revertedWith(
+        "ERR_INSUFFICIENT_BAL"
+      );
+    });
+    it("Sending the entire balance should work", async () => {
+      await pcToken.mint(account, constants.WeiPerEther);
+      await pcToken.transfer(account2, constants.WeiPerEther);
 
-  //     const accountBalance = await pcToken.balanceOf(account);
-  //     const account2Balance = await pcToken.balanceOf(account2);
+      const accountBalance = await pcToken.balanceOf(account);
+      const account2Balance = await pcToken.balanceOf(account2);
 
-  //     expect(accountBalance).to.eq(0);
-  //     expect(account2Balance).to.eq(constants.WeiPerEther);
-  //   });
-  //   it("Should emit transfer event", async () => {
-  //     // 触发转让事件
-  //     await pcToken.mint(account, constants.WeiPerEther);
-  //     await expect(pcToken.transfer(account2, constants.WeiPerEther))
-  //       .to.emit(pcToken, "Transfer")
-  //       .withArgs(account, account2, constants.WeiPerEther);
-  //   });
-  //   it("Sending 0 tokens should work", async () => {
-  //     await pcToken.mint(account, constants.WeiPerEther);
-  //     await pcToken.transfer(account2, constants.Zero);
+      expect(accountBalance).to.eq(0);
+      expect(account2Balance).to.eq(constants.WeiPerEther);
+    });
+    it("Should emit transfer event", async () => {
+      // 触发转让事件
+      await pcToken.mint(account, constants.WeiPerEther);
+      await expect(pcToken.transfer(account2, constants.WeiPerEther))
+        .to.emit(pcToken, "Transfer")
+        .withArgs(account, account2, constants.WeiPerEther);
+    });
+    it("Sending 0 tokens should work", async () => {
+      await pcToken.mint(account, constants.WeiPerEther);
+      await pcToken.transfer(account2, constants.Zero);
 
-  //     const accountBalance = await pcToken.balanceOf(account);
-  //     const account2Balance = await pcToken.balanceOf(account2);
+      const accountBalance = await pcToken.balanceOf(account);
+      const account2Balance = await pcToken.balanceOf(account2);
 
-  //     expect(accountBalance).to.eq(constants.WeiPerEther);
-  //     expect(account2Balance).to.eq(0);
-  //   });
-  // });
+      expect(accountBalance).to.eq(constants.WeiPerEther);
+      expect(account2Balance).to.eq(0);
+    });
+  });
   describe("approve", async () => {
     it("Should emit event", async () => {
       await expect(pcToken.approve(account2, constants.WeiPerEther))
@@ -206,50 +206,50 @@ describe("poolToken", function () {
       expect(approvalAmount).to.eq(constants.Zero);
     });
   });
-//   describe("transferFrom", async () => {
-//     beforeEach(async () => {
-//       await pcToken.mint(account, constants.WeiPerEther);
-//     });
-//     it("Should emit event", async () => {
-//       await pcToken.approve(account2, constants.WeiPerEther);
-//       pcToken = pcToken.connect(signers[1]);
-//       await expect(pcToken.transferFrom(account, account2, constants.WeiPerEther))
-//         .to.emit(pcToken, "Transfer")
-//         .withArgs(account, account2, constants.WeiPerEther);
-//     });
-//     it("Should work when sender has enough balance and approved spender", async () => {
-//       await pcToken.approve(account2, constants.WeiPerEther);
-//       pcToken = pcToken.connect(signers[1]);
-//       await pcToken.transferFrom(account, account2, constants.WeiPerEther);
+  describe("transferFrom", async () => {
+    beforeEach(async () => {
+      await pcToken.mint(account, constants.WeiPerEther);
+    });
+    it("Should emit event", async () => {
+      await pcToken.approve(account2, constants.WeiPerEther);
+      pcToken = pcToken.connect(signers[1]);
+      await expect(pcToken.transferFrom(account, account2, constants.WeiPerEther))
+        .to.emit(pcToken, "Transfer")
+        .withArgs(account, account2, constants.WeiPerEther);
+    });
+    it("Should work when sender has enough balance and approved spender", async () => {
+      await pcToken.approve(account2, constants.WeiPerEther);
+      pcToken = pcToken.connect(signers[1]);
+      await pcToken.transferFrom(account, account2, constants.WeiPerEther);
 
-//       const accountBalance = await pcToken.balanceOf(account);
-//       const account2Balance = await pcToken.balanceOf(account2);
-//       const approvalAmount = await pcToken.allowance(account, account2);
+      const accountBalance = await pcToken.balanceOf(account);
+      const account2Balance = await pcToken.balanceOf(account2);
+      const approvalAmount = await pcToken.allowance(account, account2);
 
-//       expect(accountBalance).to.eq(constants.Zero);
-//       expect(account2Balance).to.eq(constants.WeiPerEther);
-//       expect(approvalAmount).to.eq(constants.Zero);
-//     });
-//     it("Should fail when not enough allowance is set", async () => {
-//       await pcToken.approve(account2, constants.WeiPerEther.sub(1));
-//       pcToken = pcToken.connect(signers[1]);
-//       await expect(
-//         pcToken.transferFrom(account, account2, constants.WeiPerEther)
-//       ).to.be.revertedWith("ERR_PCTOKEN_BAD_CALLER");
-//     });
-//     it("Should fail when sender does not have enough balance", async () => {
-//       await pcToken.approve(account2, constants.WeiPerEther.add(1));
-//       pcToken = pcToken.connect(signers[1]);
-//       await expect(
-//         pcToken.transferFrom(account, account2, constants.WeiPerEther.add(1))
-//       ).to.be.revertedWith("ERR_INSUFFICIENT_BAL");
-//     });
-//     it("Should not change approval amount when it was set to max uint256", async () => {
-//       await pcToken.approve(account2, constants.MaxUint256);
-//       pcToken = pcToken.connect(signers[1]);
-//       await pcToken.transferFrom(account, account2, constants.WeiPerEther);
-//       const approvalAmount = await pcToken.allowance(account, account2);
-//       expect(approvalAmount).to.eq(constants.MaxUint256);
-//     });
-//   });
-// });
+      expect(accountBalance).to.eq(constants.Zero);
+      expect(account2Balance).to.eq(constants.WeiPerEther);
+      expect(approvalAmount).to.eq(constants.Zero);
+    });
+    it("Should fail when not enough allowance is set", async () => {
+      await pcToken.approve(account2, constants.WeiPerEther.sub(1));
+      pcToken = pcToken.connect(signers[1]);
+      await expect(
+        pcToken.transferFrom(account, account2, constants.WeiPerEther)
+      ).to.be.revertedWith("ERR_PCTOKEN_BAD_CALLER");
+    });
+    it("Should fail when sender does not have enough balance", async () => {
+      await pcToken.approve(account2, constants.WeiPerEther.add(1));
+      pcToken = pcToken.connect(signers[1]);
+      await expect(
+        pcToken.transferFrom(account, account2, constants.WeiPerEther.add(1))
+      ).to.be.revertedWith("ERR_INSUFFICIENT_BAL");
+    });
+    it("Should not change approval amount when it was set to max uint256", async () => {
+      await pcToken.approve(account2, constants.MaxUint256);
+      pcToken = pcToken.connect(signers[1]);
+      await pcToken.transferFrom(account, account2, constants.WeiPerEther);
+      const approvalAmount = await pcToken.allowance(account, account2);
+      expect(approvalAmount).to.eq(constants.MaxUint256);
+    });
+  });
+});
